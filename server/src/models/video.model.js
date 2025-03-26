5ebc4e7dbd075afb28d531ae57e48ca1038c8c30
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 22:27:54 +0500

    Enhance video schema: add new fields for videoId, tags, category, likes, and dislikesCount, while streamlining existing fields with trim option for better data handling.

--- server/src/models/video.model.js
+++ server/src/models/video.model.js
@@ -3,43 +3,21 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
-    
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    videoUrl: {
-      type: String,
-      required: true,
-    },
-    thumbnailUrl: {
-      type: String,
-      required: true,
-    },
-    thumbnailId:{ type: String,
-      required: true
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    views: {
-      type: Number,
-      default: 0,
-      min:0
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    videoUrl: { type: String, required: true },
+    videoId: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    thumbnailId: { type: String, required: true },
+    tag: [{ type: String, trim: true }],
+    category: { type: String, required: true, trim: true },
+    duration: { type: Number, required: true },
+    views: { type: Number, default: 0, min: 0 },
+    likes: { type: Number, default: 0, min: 0 },
+    dislikesCount: { type: Number, default: 0 },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 videoSchema.plugin(mongooseAggregatePaginate);
