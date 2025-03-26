ebecb303b28460875ec2a7fea4fc312b72c9342c
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 19:41:04 +0500

    Add subscription route: integrate subscriptionRouter to handle subscription-related API requests at /api/v1/subscribe

--- server/src/app.js
+++ server/src/app.js
@@ -18,8 +18,12 @@ app.use(cookieParser());
 
 //router import
 import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js"
+
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscribe", subscriptionRouter);
+
 
 export { app };
