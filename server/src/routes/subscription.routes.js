e3d5fdf163148f0097cb3505dba1cd3af46d7d14
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 19:41:11 +0500

    Add subscription routes: create a new subscription.routes.js file to define secure API endpoints for managing subscriptions with JWT verification.

---
+++ server/src/routes/subscription.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import { verifyJwt } from "../middlewares/auth.middlewares.js";
+import { subscribeAndUnsubscribe } from "../controllers/subscription.model.js";
+
+const router = Router();
+
+//secure routes
+router.route("/:channelName").post(verifyJwt,subscribeAndUnsubscribe);
+
+
+
+
+export default router;
