8b28db2af519355c48289b8a6ef821310743efe8
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 19:40:58 +0500

    Add subscription functionality: implement subscribeAndUnsubscribe controller to manage user subscriptions, including error handling for self-subscription and existing subscriptions.

---
+++ server/src/controllers/subscription.model.js
@@ -0,0 +1,39 @@
+import { Subscription } from "../models/subscription.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const subscribeAndUnsubscribe = asyncHandler(async (req, res) => {
+  const { channelName } = req.params; 
+  const userId = req.user._id; 
+
+
+  const channel = await User.findOne({ username: channelName.toLowerCase() });
+
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  if (userId.toString() === channel._id.toString()) {
+    throw new ApiError(400, "You cannot subscribe to your own channel.");
+  }
+
+  
+  const existingSubscription = await Subscription.findOne({
+    subscriber: userId,
+    channel: channel._id,
+  });
+
+  if (existingSubscription) {
+    await Subscription.deleteOne({ _id: existingSubscription._id });
+    return res.status(200).json({ message: `Unsubscribed from ${channelName} successfully` });
+  } else {
+    const newSubscription = new Subscription({
+      subscriber: userId,
+      channel: channel._id,
+    });
+
+    await newSubscription.save();
+    return res.status(201).json({ message: `Subscribed to ${channelName} successfully` });
+  }
+});
