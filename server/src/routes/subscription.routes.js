1f764c3d4fbf63c6793590cc92cd2bbe9dc847e9
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 21:20:01 +0500

    Refactor subscription routes: update import path for subscribeAndUnsubscribe function and clean up code formatting for improved readability.

--- server/src/routes/subscription.routes.js
+++ server/src/routes/subscription.routes.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
 
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
-import { subscribeAndUnsubscribe } from "../controllers/subscription.model.js";
+import { subscribeAndUnsubscribe } from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 //secure routes
-router.route("/:channelName").post(verifyJwt,subscribeAndUnsubscribe);
-
-
-
+router.route("/:channelName").post(verifyJwt, subscribeAndUnsubscribe);
 
 export default router;
