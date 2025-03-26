33a7b705bd4417f7c9d558c72ee0b4295d625427
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-26 22:27:40 +0500

    Add video routes: integrate videoRouter to handle API requests at /api/v1/video for video management.

--- server/src/app.js
+++ server/src/app.js
@@ -19,11 +19,15 @@ app.use(cookieParser());
 //router import
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+
 
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscribe", subscriptionRouter);
+app.use("/api/v1/video", videoRouter);
+
 
 
 export { app };
